63f5e014222fe5396419e2075126b2436dab13c1
wikmor <wmmorski@gmail.com>
2024-03-16 16:38:17 +0100

    Add get_documents_txt function

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,15 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders.text import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+
+def get_documents_txt(path):
+    loader = TextLoader(path)
+    documents = loader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    return text_splitter.split_documents(documents)
 
 
 def get_documents_pdf(path):
