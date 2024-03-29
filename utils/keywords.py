f1101be69950f8c989e88ef6f0c0c3df4fd7508d
Przemysław Sipa <przemeksipa14@gmail.com>
2024-03-29 08:47:08 +0100

    [keywords] add reading from file

--- utils/keywords.py
+++ utils/keywords.py
@@ -1,12 +1,17 @@
 import pke 
-def search_keywords(text:str) -> list[str]:
+def search_keyphrases(path:str) -> list[str]:
     """ Extracts keywords from the text using TextRank algorithm.
     Args:
-        text (str): Text from which keywords are to be extracted.
+        path (str): Path to the text file.
     Returns:
         keyphrases (list): List of keyphrases extracted from the text.
         keywords (list): List of keywords extracted from the text.
         """
+        
+    open_file = open(path, "r")
+    text = open_file.read()
+        
+        
     part_of_speech_tags: set = {'NOUN', 'PROPN', 'ADJ'}
 
     # 1. create a TextRank extractor.
@@ -21,31 +26,12 @@ def search_keywords(text:str) -> list[str]:
     #    Keyphrase candidates are composed from the 33-percent
     #    highest-ranked words.
     extractor.candidate_weighting(window=2,
-                                pos=pos,
+                                pos=part_of_speech_tags,
                                 top_percent=0.33)
 
     # 4. get the 10-highest scored candidates as keyphrases
     keyphrases = extractor.get_n_best(n=10)
     return [keyword for keyword, score in keyphrases]
-text = """
-Graph-based ranking algorithms are essentially a
-way of deciding the importance of a vertex within
-a graph, based on global information recursively
-drawn from the entire graph. The basic idea implemented by a graph-based ranking model is that
-of “voting” or “recommendation”. When one vertex links to another one, it is basically casting a vote
-for that other vertex. The higher the number of votes
-that are cast for a vertex, the higher the importance
-of the vertex. Moreover, the importance of the vertex
-casting the vote determines how important the vote
-itself is, and this information is also taken into account by the ranking model. Hence, the score associated with a vertex is determined based on the votes
-that are cast for it, and the score of the vertices casting these votes.
-"""
-print(search_keywords(text))
 
-
-#output([('basic idea', 0.15267204128774325),
-# ('entire graph', 0.15267200128774328), 
-# ('global information', 0.15267193128774326), ('ranking', 0.11194630201412213),
-# ('information', 0.07633704064387163), ('graph', 0.07633604064387163),
-# ('vertex', 0.07633600064387164)],
-# ['basic idea', 'entire graph', 'global information', 'ranking', 'information', 'graph', 'vertex'])
+path_to_file ="examples/summ_text.txt"
+print(search_keyphrases(path_to_file))
