ee97af5bab1286a73489c0f552f4502c267a931f
Przemysław Sipa <przemeksipa14@gmail.com>
2024-04-17 23:38:33 +0200

    pwa: change structure to fastapi static files

--- app/main.py
+++ app/main.py
@@ -1,19 +1,29 @@
 from typing import Union
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static") # static files
+templates = Jinja2Templates(directory="templates") # templates
 
+# main page
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    response = templates.TemplateResponse("index_main.html", {"request": request})
+    return response
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# result page
+@app.get('/result', response_class=HTMLResponse)
+async def result(request: Request):
+    response = templates.TemplateResponse("result_page.html", {"request": request})
+    return response
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
+        # działa na 127.0.0.1:8000
+    # terminal musi być na direcory app tzn. working directory to app
 #run uvicorn main:app --reload
