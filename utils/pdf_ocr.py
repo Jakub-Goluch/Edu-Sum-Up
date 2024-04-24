80e957c22be20ad39d4d898ac0c62ae161fd003a
Jakub-Goluch <99048106+Jakub-Goluch@users.noreply.github.com>
2024-04-24 20:53:33 +0200

    remove pytesseract from requirements and imports

--- utils/pdf_ocr.py
+++ utils/pdf_ocr.py
@@ -1,12 +1,10 @@
-import pytesseract
 from fpdf import FPDF
 from pdf2image import convert_from_path
 import easyocr
 import numpy as np
 
+
 # pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe" # PATH to a tesseract exe
-
-
 
 def delete_special_characters(word):
     for character in word:
@@ -40,7 +38,8 @@ def delete_special_characters(word):
 def convert_images_to_searchable_pdf(pdf_path: str, return_path: str) -> None:
     """Extracts text from an image and saves it as pdf file to user device"""
     reader = easyocr.Reader(['en', 'pl'])  # this needs to run only once to load the model into memory
-    images = convert_from_path(pdf_path, poppler_path="C:\\Program Files\\Poppler\\poppler-24.02.0\\Library\\bin") # PATH to a bin folder of Poppler
+    images = convert_from_path(pdf_path,
+                               poppler_path="C:\\Program Files\\Poppler\\poppler-24.02.0\\Library\\bin")  # PATH to a bin folder of Poppler
     pdf = FPDF()
     for i, image in enumerate(images):
         image_array = np.array(image)
@@ -56,8 +55,3 @@ def convert_images_to_searchable_pdf(pdf_path: str, return_path: str) -> None:
                 pdf.cell(200, 10, txt=line, ln=1, align='C')
 
     pdf.output(return_path)
-
-pdf1 = "C:\\Users\\Kuba\\Desktop\\Python\\Edu-Sum-Up\\examples\\artykul.pdf"
-pdf2 = "C:\\Users\\Kuba\\Desktop\\Python\\Edu-Sum-Up\\examples\\artykul_2.pdf"
-
-convert_images_to_searchable_pdf(pdf1, pdf2)
